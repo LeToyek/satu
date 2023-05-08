e36f0386540dfcb38c91deed648ed91bce652ddb
Tio Misbaqul Irawan <sattanationmail@gmail.com>
2023-05-08 09:37:29 +0000

    feat: document model

--- app/Models/Partner.php
+++ app/Models/Partner.php
@@ -4,8 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 
 class Partner extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'name',
+        'address',
+        'found_at',
+        'sector',
+        'monthly_income',
+        'grade'
+    ];
+
+    public function documents(): MorphMany
+    {
+        return $this->morphMany(Document::class, 'documentable');
+    }
 }
