cefd302daf9b2d3bd79f4c9f0bee968c828c94c0
= <maulanatoyek@gmail.com>
2023-05-08 15:34:39 +0700

    feat: init velzon

---
+++ resources/views/dashboard/layouts/head-css.blade.php
@@ -0,0 +1,11 @@
+@yield('css')
+<!-- Layout config Js -->
+<script src="{{ URL::asset('velzon/js/layout.js') }}"></script>
+<!-- Bootstrap Css -->
+<link href="{{ URL::asset('velzon/css/bootstrap.min.css') }}" id="bootstrap-style" rel="stylesheet" type="text/css" />
+<!-- Icons Css -->
+<link href="{{ URL::asset('velzon/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
+<!-- App Css-->
+<link href="{{ URL::asset('velzon/css/app.min.css') }}" id="app-style" rel="stylesheet" type="text/css" />
+<!-- custom Css-->
+<link href="{{ URL::asset('velzon/css/custom.min.css') }}" id="app-style" rel="stylesheet" type="text/css" />
--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('pages.home');
-});
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', function () {
+    return view('pages.home');
+});
+Route::get('/dashboard', function () {
+    return view('dashboard.pages.index');
+});
