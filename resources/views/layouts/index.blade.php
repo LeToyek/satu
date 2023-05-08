bdb79c2cfd196a7cee7fb44a8f3b7802ba16b1dc
= <maulanatoyek@gmail.com>
2023-05-08 14:20:05 +0700

    feat: init layouts

---
+++ resources/views/layouts/index.blade.php
@@ -0,0 +1,56 @@
+<html>
+
+<head>
+    <!--====== Required meta tags ======-->
+    <meta charset="utf-8" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge" />
+    <meta name="description" content="" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+    <!--====== Title ======-->
+    <title> Funden - Crowdfunding & Charity HTML Template || Home One </title>
+
+    <!--====== Favicon Icon ======-->
+    <link rel="shortcut icon" href="{{ asset('assets/img/favicon.ico') }}" type="img/png" />
+    <!--====== Animate Css ======-->
+    <link rel="stylesheet" href="{{ asset('assets/css/animate.min.css') }}">
+    <!--====== Bootstrap css ======-->
+    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}" />
+    <!--====== Fontawesome css ======-->
+    <link rel="stylesheet" href="{{ asset('assets/css/font-awesome.min.css') }}" />
+    <!--====== Flaticon css ======-->
+    <link rel="stylesheet" href="{{ asset('assets/css/flaticon.css') }}" />
+    <!--====== Slick Css ======-->
+    <link rel="stylesheet" href="{{ asset('assets/css/slick.min.css') }}" />
+    <!--====== Lity Css ======-->
+    <link rel="stylesheet" href="{{ asset('assets/css/lity.min.css') }}" />
+    <!--====== Main css ======-->
+    <link rel="stylesheet" href="{{ asset('assets/css/main.css') }}" />
+    <!--====== Responsive css ======-->
+    <link rel="stylesheet" href="{{ asset('assets/css/responsive.css') }}" />
+</head>
+
+<body>
+
+    @include('layouts.navbar')
+    @yield('container')
+    @include('layouts.footer')
+
+    <!--====== jquery js ======-->
+    <script src="{{ 'assets/js/jquery.min.js' }}"></script>
+    <!--====== Bootstrap js ======-->
+    <script src="{{ 'assets/js/bootstrap.min.js' }}"></script>
+    <!--====== Inview js ======-->
+    <script src="{{ 'assets/js/jquery.inview.min.js' }}"></script>
+    <!--====== Slick js ======-->
+    <script src="{{ 'assets/js/slick.min.js' }}"></script>
+    <!--====== Lity js ======-->
+    <script src="{{ 'assets/js/lity.min.js' }}"></script>
+    <!--====== Wow js ======-->
+    <script src="{{ 'assets/js/wow.min.js' }}"></script>
+    <!--====== Main js ======-->
+    <script src="{{ 'assets/js/main.js' }}"></script>
+
+</body>
+
+</html>
---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,59 @@
+<div class="navbar-wrapper">
+  <div class="container">
+    <div class="navbar-inner">
+      <div class="site-logo">
+        <a href="index.html"><img src="assets/img/logo.png" alt="Funden"></a>
+      </div>
+      <div class="nav-menu">
+        <ul>
+          <li class="current">
+            <a href="index.html">Home</a>
+            <ul class="submenu">
+              <li><a href="index.html">Home One</a></li>
+              <li><a href="index-2.html">Home Two</a></li>
+            </ul>
+          </li>
+          <li>
+            <a href="project-1.html">Project</a>
+            <ul class="submenu">
+              <li><a href="project-1.html">Project One</a></li>
+              <li><a href="project-2.html">Project Two</a></li>
+              <li><a href="project-3.html">Project Three</a></li>
+              <li><a href="project-details.html">Project Details</a></li>
+            </ul>
+          </li>
+          <li>
+            <a href="events.html">Events</a>
+          </li>
+          <li>
+            <a href="news-standard.html">News</a>
+            <ul class="submenu">
+              <li><a href="news-standard.html">News Standard</a></li>
+              <li><a href="news-details.html">News Details</a></li>
+            </ul>
+          </li>
+          <li>
+            <a href="#">Pages</a>
+            <ul class="submenu">
+              <li><a href="about.html">About</a></li>
+              <li><a href="company-overview.html">Company Overview</a></li>
+              <li><a href="team-member.html">Team Member</a></li>
+              <li><a href="pricing.html">Pricing</a></li>
+              <li><a href="faq.html">FAQ</a></li>
+              <li><a href="testimonial.html">Testimonials</a></li>
+            </ul>
+          </li>
+          <li><a href="contact.html">Contact</a></li>
+        </ul>
+      </div>
+      <div class="navbar-extra d-flex align-items-center">
+        <a href="events.html" class="main-btn nav-btn d-none d-sm-inline-block">
+          Donate Now <i class="far fa-arrow-right"></i>
+        </a>
+        <a href="#" class="nav-toggler">
+                        <span></span>
+                    </a>
+      </div>
+    </div>
+  </div>
+</div>
