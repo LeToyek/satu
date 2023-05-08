f5a2b45db9bba344f83a033cd88ebb0dd00fafe2
Tio Misbaqul Irawan <sattanationmail@gmail.com>
2023-05-08 10:30:53 +0000

    feat: user detail relations

--- app/Models/Partner.php
+++ app/Models/Partner.php
@@ -20,6 +20,11 @@ class Partner extends Model
         'grade'
     ];
 
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
     public function documents(): MorphMany
     {
         return $this->morphMany(Document::class, 'documentable');
