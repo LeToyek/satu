f18708e7864c9333e9567c4687266c192c8566eb
lailatulbadriyah24 <lailatulbadriyah654@gmail.com>
2023-05-10 21:48:51 +0700

    feat: fix title

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 
     <!--====== Title ======-->
-    <title> Funden - Crowdfunding & Charity HTML Template || Home One </title>
+    <title> SaTu - Saling Bantu </title>
 
     <!--====== Favicon Icon ======-->
     <link rel="shortcut icon" href="{{ asset('assets/img/favicon.ico') }}" type="img/png" />
