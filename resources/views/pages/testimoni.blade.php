81a59008f7d2f0853923b55282788a50677aca24
lailatulbadriyah24 <lailatulbadriyah654@gmail.com>
2023-05-10 22:05:02 +0700

    feat: fix testimoni

--- resources/views/pages/testimoni.blade.php
+++ resources/views/pages/testimoni.blade.php
@@ -1,197 +1,207 @@
 @extends('layouts.index')
 @section('container')
+    <!--====== Page Title Start ======-->
+    <section class="page-title-area">
+        <div class="container">
+            <div class="row align-items-center justify-content-between">
+                <div class="col-lg-8">
+                    <h1 class="page-title">Testimoni</h1>
+                </div>
+                <div class="col-auto">
+                    <ul class="page-breadcrumb">
+                        <li><a href="index.html">Home</a></li>
+                        <li>Testimoni</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--====== Page Title End ======-->
 
-	<!--====== Page Title Start ======-->
-	<section class="page-title-area">
-		<div class="container">
-			<div class="row align-items-center justify-content-between">
-				<div class="col-lg-8">
-					<h1 class="page-title">Testimoni</h1>
-				</div>
-				<div class="col-auto">
-					<ul class="page-breadcrumb">
-						<li><a href="index.html">Home</a></li>
-						<li>Testimoni</li>
-					</ul>
-				</div>
-			</div>
-		</div>
-	</section>
-	<!--====== Page Title End ======-->
 
-	
 
-	
-	
-	<!--====== Testimonials Start ======-->
-	<section class="testimonials-section section-gap">
-		<div class="container">
-			<div class="common-heading text-center mb-30">
-				<span class="tagline">
-					<i class="fas fa-plus"></i> Feedback Mitra
-					<span class="heading-shadow-text">Testimoni</span>
-				</span>
-				<h2 class="title">Apa Kata Mereka</h2>
-			</div>
 
-			<div class="row justify-content-center testimonial-boxes square-shape-two">
-				<div class="col-lg-4 col-md-6 col-sm-10 wow fadeInUp" data-wow-delay="0.2s">
-					<div class="testimonial-box-one mt-30">
-						<div class="author-info">
-							<div class="author-img">
-								<img src="assets/img/author-thumbs/org1.jpg" alt="Thumb">
-							</div>
-							<div>
-								<h5 class="name">Budi Irawan</h5>
-								<p class="position">Pemilik Toko Baju</p>
-							</div>
-						</div>
-						<p class="testimonial-desc">
-							Dengan SaTu, kampanye penggalangan dana saya menjadi lebih mudah dan efisien. Saya berhasil mengumpulkan dana yang cukup untuk memulai bisnis saya yang baru. Saya sangat senang. Terima kasih SaTu!
-						</p>
-						<div class="rating-wrap">
-							<span>Rating</span>
-							<ul>
-								<li><i class="fas fa-star"></i></li>
-								<li><i class="fas fa-star"></i></li>
-								<li><i class="fas fa-star"></i></li>
-								<li><i class="fas fa-star"></i></li>
-								<li><i class="fas fa-star"></i></li>
-							</ul>
-						</div>
-					</div>
-				</div>
-				<div class="col-lg-4 col-md-6 col-sm-10 wow fadeInUp" data-wow-delay="0.3s">
-					<div class="testimonial-box-one mt-30">
-						<div class="author-info">
-							<div class="author-img">
-								<img src="assets/img/author-thumbs/org2.jpg" alt="Thumb">
-							</div>
-							<div>
-								<h5 class="name">Deni Sudrajat</h5>
-								<p class="position">Pemilik Warung Kopi</p>
-							</div>
-						</div>
-						<p class="testimonial-desc">
-							SaTu membantu saya mengumpulkan dana dari banyak orang dengan mudah dan meningkatkan visibilitas kampanye saya. Sangat direkomendasikan untuk yang ingin menggalang dana dengan cepat dan mudah.
-						</p>
-						<div class="rating-wrap">
-							<span>Rating</span>
-							<ul>
-								<li><i class="fas fa-star"></i></li>
-								<li><i class="fas fa-star"></i></li>
-								<li><i class="fas fa-star"></i></li>
-								<li><i class="fas fa-star"></i></li>
-								<li><i class="fas fa-star"></i></li>
-							</ul>
-						</div>
-					</div>
-				</div>
-				<div class="col-lg-4 col-md-6 col-sm-10 wow fadeInUp" data-wow-delay="0.4s">
-					<div class="testimonial-box-one mt-30">
-						<div class="author-info">
-							<div class="author-img">
-								<img src="assets/img/author-thumbs/org3.jpg" alt="Thumb">
-							</div>
-							<div>
-								<h5 class="name">Rina Ariani</h5>
-								<p class="position">Pemilik Katering</p>
-							</div>
-						</div>
-						<p class="testimonial-desc">
-							Saya memiliki ide bisnis yang luar biasa, tetapi saya tidak punya modal untuk memulainya. SaTu membantu saya mengumpulkan dana dan sekarang bisnis saya sudah menjadi sukses dan semakin berkembang.
-						</p>
-						<div class="rating-wrap">
-							<span>Rating</span>
-							<ul>
-								<li><i class="fas fa-star"></i></li>
-								<li><i class="fas fa-star"></i></li>
-								<li><i class="fas fa-star"></i></li>
-								<li><i class="fas fa-star"></i></li>
-								<li><i class="fas fa-star"></i></li>
-							</ul>
-						</div>
-					</div>
-				</div>
-				<div class="col-lg-4 col-md-6 col-sm-10 wow fadeInUp" data-wow-delay="0.5s">
-					<div class="testimonial-box-one mt-30">
-						<div class="author-info">
-							<div class="author-img">
-								<img src="assets/img/author-thumbs/org4.jpg" alt="Thumb">
-							</div>
-							<div>
-								<h5 class="name">Joko Handoko</h5>
-								<p class="position">Pemilik Bengkel Motor</p>
-							</div>
-						</div>
-						<p class="testimonial-desc">
-							Saya ingin membuka usaha bengkel motor, tetapi modal yang saya miliki tidak cukup. SaTu membantu saya mengumpulkan dana dari banyak orang dan sekarang usaha bengkel motor saya sudah berjalan dengan lancar.
-						</p>
-						<div class="rating-wrap">
-							<span>Rating</span>
-							<ul>
-								<li><i class="fas fa-star"></i></li>
-								<li><i class="fas fa-star"></i></li>
-								<li><i class="fas fa-star"></i></li>
-								<li><i class="fas fa-star"></i></li>
-								<li><i class="fas fa-star"></i></li>
-							</ul>
-						</div>
-					</div>
-				</div>
-				<div class="col-lg-4 col-md-6 col-sm-10 wow fadeInUp" data-wow-delay="0.6s">
-					<div class="testimonial-box-one mt-30">
-						<div class="author-info">
-							<div class="author-img">
-								<img src="assets/img/author-thumbs/org5.jpg" alt="Thumb">
-							</div>
-							<div>
-								<h5 class="name">Maharani Aliana</h5>
-								<p class="position">Pemilik Toko Bunga</p>
-							</div>
-						</div>
-						<p class="testimonial-desc">
-							Saya sangat terkesan dengan layanan yang diberikan oleh SaTu. Mereka memberikan dukungan yang sangat baik dan responsif dalam setiap tahap proses penggalangan dana yang membuat proyek saya lancar. Terima kasih SaTu! 
-						</p>
-						<div class="rating-wrap">
-							<span>Rating</span>
-							<ul>
-								<li><i class="fas fa-star"></i></li>
-								<li><i class="fas fa-star"></i></li>
-								<li><i class="fas fa-star"></i></li>
-								<li><i class="fas fa-star"></i></li>
-								<li><i class="fas fa-star"></i></li>
-							</ul>
-						</div>
-					</div>
-				</div>
-				<div class="col-lg-4 col-md-6 col-sm-10 wow fadeInUp" data-wow-delay="0.7s">
-					<div class="testimonial-box-one mt-30">
-						<div class="author-info">
-							<div class="author-img">
-								<img src="assets/img/author-thumbs/org6.jpg" alt="Thumb">
-							</div>
-							<div>
-								<h5 class="name">Dea Anugrah</h5>
-								<p class="position">Pemilik Toko Roti</p>
-							</div>
-						</div>
-						<p class="testimonial-desc">
-							SaTu sangat efisien dalam menggalang dana dan memberikan tingkat keamanan yang baik pada setiap transaksi. Saya merekomendasikan SaTu untuk para pengusaha yang ingin menggalang dana dengan aman dan efisien.
-						<div class="rating-wrap">
-							<span>Rating</span>
-							<ul>
-								<li><i class="fas fa-star"></i></li>
-								<li><i class="fas fa-star"></i></li>
-								<li><i class="fas fa-star"></i></li>
-								<li><i class="fas fa-star"></i></li>
-								<li><i class="fas fa-star"></i></li>
-							</ul>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</section>
-	<!--====== Testimonials End ======-->
-    @endsection
+
+    <!--====== Testimonials Start ======-->
+    <section class="testimonials-section section-gap">
+        <div class="container">
+            <div class="common-heading text-center mb-30">
+                <span class="tagline">
+                    <i class="fas fa-plus"></i> Feedback Mitra
+                    <span class="heading-shadow-text">Testimoni</span>
+                </span>
+                <h2 class="title">Apa Kata Mereka</h2>
+            </div>
+
+            <div class="row justify-content-center testimonial-boxes square-shape-two">
+                <div class="col-lg-4 col-md-6 col-sm-10 wow fadeInUp" data-wow-delay="0.2s">
+                    <div class="testimonial-box-one mt-30">
+                        <div class="author-info">
+                            <div class="author-img">
+                                <img src="assets/img/author-thumbs/org1.jpg" alt="Thumb">
+                            </div>
+                            <div>
+                                <h5 class="name">Budi Irawan</h5>
+                                <p class="position">Pemilik Toko Baju</p>
+                            </div>
+                        </div>
+                        <p class="testimonial-desc">
+                            Dengan SaTu, kampanye penggalangan dana saya menjadi lebih mudah dan efisien. Saya berhasil
+                            mengumpulkan dana yang cukup untuk memulai bisnis saya yang baru. Saya sangat senang. Terima
+                            kasih SaTu!
+                        </p>
+                        <div class="rating-wrap">
+                            <span>Rating</span>
+                            <ul>
+                                <li><i class="fas fa-star"></i></li>
+                                <li><i class="fas fa-star"></i></li>
+                                <li><i class="fas fa-star"></i></li>
+                                <li><i class="fas fa-star"></i></li>
+                                <li><i class="fas fa-star"></i></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-10 wow fadeInUp" data-wow-delay="0.3s">
+                    <div class="testimonial-box-one mt-30">
+                        <div class="author-info">
+                            <div class="author-img">
+                                <img src="assets/img/author-thumbs/org2.jpg" alt="Thumb">
+                            </div>
+                            <div>
+                                <h5 class="name">Deni Sudrajat</h5>
+                                <p class="position">Pemilik Warung Kopi</p>
+                            </div>
+                        </div>
+                        <p class="testimonial-desc">
+                            SaTu membantu saya mengumpulkan dana dari banyak orang dengan mudah dan meningkatkan visibilitas
+                            kampanye saya. Sangat direkomendasikan untuk yang ingin menggalang dana dengan cepat dan mudah.
+                        </p>
+                        <div class="rating-wrap">
+                            <span>Rating</span>
+                            <ul>
+                                <li><i class="fas fa-star"></i></li>
+                                <li><i class="fas fa-star"></i></li>
+                                <li><i class="fas fa-star"></i></li>
+                                <li><i class="fas fa-star"></i></li>
+                                <li><i class="fas fa-star"></i></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-10 wow fadeInUp" data-wow-delay="0.4s">
+                    <div class="testimonial-box-one mt-30">
+                        <div class="author-info">
+                            <div class="author-img">
+                                <img src="assets/img/author-thumbs/org3.jpg" alt="Thumb">
+                            </div>
+                            <div>
+                                <h5 class="name">Rina Ariani</h5>
+                                <p class="position">Pemilik Katering</p>
+                            </div>
+                        </div>
+                        <p class="testimonial-desc">
+                            Saya memiliki ide bisnis yang luar biasa, tetapi saya tidak punya modal untuk memulainya. SaTu
+                            membantu saya mengumpulkan dana dan sekarang bisnis saya sudah menjadi sukses dan semakin
+                            berkembang.
+                        </p>
+                        <div class="rating-wrap">
+                            <span>Rating</span>
+                            <ul>
+                                <li><i class="fas fa-star"></i></li>
+                                <li><i class="fas fa-star"></i></li>
+                                <li><i class="fas fa-star"></i></li>
+                                <li><i class="fas fa-star"></i></li>
+                                <li><i class="fas fa-star"></i></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-10 wow fadeInUp" data-wow-delay="0.5s">
+                    <div class="testimonial-box-one mt-30">
+                        <div class="author-info">
+                            <div class="author-img">
+                                <img src="assets/img/author-thumbs/org4.jpg" alt="Thumb">
+                            </div>
+                            <div>
+                                <h5 class="name">Joko Handoko</h5>
+                                <p class="position">Pemilik Bengkel Motor</p>
+                            </div>
+                        </div>
+                        <p class="testimonial-desc">
+                            Saya ingin membuka usaha bengkel motor, tetapi modal yang saya miliki tidak cukup. SaTu membantu
+                            saya mengumpulkan dana dari banyak orang dan sekarang usaha bengkel motor saya sudah berjalan
+                            dengan lancar.
+                        </p>
+                        <div class="rating-wrap">
+                            <span>Rating</span>
+                            <ul>
+                                <li><i class="fas fa-star"></i></li>
+                                <li><i class="fas fa-star"></i></li>
+                                <li><i class="fas fa-star"></i></li>
+                                <li><i class="fas fa-star"></i></li>
+                                <li><i class="fas fa-star"></i></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-10 wow fadeInUp" data-wow-delay="0.6s">
+                    <div class="testimonial-box-one mt-30">
+                        <div class="author-info">
+                            <div class="author-img">
+                                <img src="assets/img/author-thumbs/org5.jpg" alt="Thumb">
+                            </div>
+                            <div>
+                                <h5 class="name">Maharani Aliana</h5>
+                                <p class="position">Pemilik Toko Bunga</p>
+                            </div>
+                        </div>
+                        <p class="testimonial-desc">
+                            Saya sangat terkesan dengan layanan yang diberikan oleh SaTu. Mereka memberikan dukungan yang
+                            sangat baik dan responsif dalam setiap tahap proses penggalangan dana yang membuat proyek saya
+                            lancar. Terima kasih SaTu!
+                        </p>
+                        <div class="rating-wrap">
+                            <span>Rating</span>
+                            <ul>
+                                <li><i class="fas fa-star"></i></li>
+                                <li><i class="fas fa-star"></i></li>
+                                <li><i class="fas fa-star"></i></li>
+                                <li><i class="fas fa-star"></i></li>
+                                <li><i class="fas fa-star"></i></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-10 wow fadeInUp" data-wow-delay="0.7s">
+                    <div class="testimonial-box-one mt-30">
+                        <div class="author-info">
+                            <div class="author-img">
+                                <img src="assets/img/author-thumbs/org6.jpg" alt="Thumb">
+                            </div>
+                            <div>
+                                <h5 class="name">Dea Anugrah</h5>
+                                <p class="position">Pemilik Toko Roti</p>
+                            </div>
+                        </div>
+                        <p class="testimonial-desc">
+                            SaTu sangat efisien dalam menggalang dana dan memberikan tingkat keamanan yang baik pada setiap
+                            transaksi. Saya merekomendasikan SaTu untuk para pengusaha yang ingin menggalang dana dengan
+                            aman dan efisien.
+                        <div class="rating-wrap">
+                            <span>Rating</span>
+                            <ul>
+                                <li><i class="fas fa-star"></i></li>
+                                <li><i class="fas fa-star"></i></li>
+                                <li><i class="fas fa-star"></i></li>
+                                <li><i class="fas fa-star"></i></li>
+                                <li><i class="fas fa-star"></i></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--====== Testimonials End ======-->
+@endsection
