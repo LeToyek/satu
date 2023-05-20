ffe417f6d2357fbc8a66c94834847f5b91ed76b4
lailatulbadriyah24 <lailatulbadriyah654@gmail.com>
2023-05-20 11:36:55 +0700

    feat: add halaman artikel

---
+++ resources/views/pages/news3.blade.php
@@ -0,0 +1,175 @@
+@extends('layouts.index')
+@section('container')
+    <!--====== Page Title Start ======-->
+    <section class="page-title-area">
+        <div class="container">
+            <div class="row align-items-center justify-content-between">
+                <div class="col-lg-8">
+                    <h1 class="page-title">Detail Artikel</h1>
+                </div>
+                <div class="col-auto">
+                    <ul class="page-breadcrumb">
+                        <li><a href="index.html">Home</a></li>
+                        <li>Detail Artikel</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--====== Page Title End ======-->
+
+    <!--====== Blog Details Area Start ======-->
+    <section class="blog-area section-gap-extra-bottom primary-soft-bg" style="padding-bottom: 110px">
+        <div class="container">
+            <div class="row row justify-content-center">
+                <div class="col-lg-8">
+                    <div class="blog-post-details text-left">
+                        <div class="post-thumbnail">
+                            <img src="../assets/img/blog/cover3.jpg" alt="Thumbnail">
+                        </div>
+                        <div class="post-content">
+                            <ul class="post-meta">
+                                <li>
+                                    <a href="#"><i class="far fa-user-circle"></i>Tim SaTu</a>
+                                </li>
+                                <li>
+                                    <a href="#"><i class="far fa-calendar-alt"></i>15 Mei 2023</a>
+                                </li>
+                            </ul>
+                            <h3 class="title">
+                                Simak Cara Membuat Surat Izin Usaha Kecil Secara Online
+                            </h3>
+                            <p>
+                                Teknologi saat ini sudah semakin memudahkan, hingga cara membuat surat izin usaha kecil
+                                dapat dilakukan secara online. Sehingga jadi lebih memudahkan bagi para pelaku usaha dalam
+                                mendaftarkan legalitas bisnisnya. Apakah kamu termasuk dari salah satu pengusaha mikro yang
+                                hendak mengurus surat tersebut?
+
+                                Sebaiknya memang jangan melakukan penundaan dalam mengurus surat izin usaha. Izin Usaha
+                                Mikro Kecil (IUMK) dapat dibuat di aplikasi bertajuk OSS atau Online Single Submission. Nah,
+                                bagaimana langkah-langkahnya? Simak petunjuknya di bawah ini!
+                            </p>
+                            <blockquote>
+                                <p>
+                                    Saat ini, internet adalah jalan terbaik untuk memperkenalkan bisnis Anda. Jadilah hadir
+                                    di sana.
+                                </p>
+                                <cite>Hadi Wenas, Pendiri Berrybenka</cite>
+                            </blockquote>
+                            <p>
+                            <h2>Cara Membuat Surat Izin Usaha Kecil Secara Online</h2><br>
+                            <p>Mengapa penting bagi bisnis untuk memiliki IUMK? Salah satunya adalah agar memiliki jaminan
+                                legalitas hukum. Sehingga, usahamu termasuk resmi atau legal dan dapat
+                                dipertanggungjawabkan.
+
+                                Selain itu, dengan memiliki IUMK akan lebih mudah bagi usaha mikro untuk mendapatkan
+                                permodalan. Berikut ini adalah cara-cara membuat surat izin usaha kecil online yang bisa
+                                kamu ikuti!</p><br>
+                            <b>1. Siapkan Persyaratan Dokumennya</b><br>
+                            Cara membuat surat izin usaha kecil yang pertama, kamu harus menyiapkan dokumen-dokumen
+                            persyaratannya. Dokumen yang dibutuhkan adalah KTP, NIB, NPWP, KK, Pas Foto, Surat Pengantar
+                            dari RT/RW dari lokasi usaha.
+
+                            Apabila sudah menyiapkan sejak awal, maka saat pengisian formulir secara online menjadi lebih
+                            lancar. Jadi, jangan sampai melewatkan dokumen di atas ya.
+                            <br>
+                            <b>2. Membuat Akun di OSS</b><br>
+                            Untuk dapat membuat akun di website resmi OSS, kunjungi www.osss.go.id/pas/ dan klik pada menu
+                            ‘Daftar’ di sisi atas sebelah kanan. Lalu, kamu bisa mengisikan data yang diminta dengan
+                            informasi yang benar.
+
+                            Jangan lupa masukkan kode captcha yang muncul sebelum klik button ‘daftar’ di sisi bawahnya.
+                            Setelah itu, kamu bisa membuka e-mail yang sudah kamu daftarkan untuk melakukan aktivasi akun.
+
+                            Akun pun teraktivasi, selanjutnya kamu harus melakukan verifikasi. Kamu akan menerima password
+                            untuk mengakses akun OSS-mu via e-mail. Simpanlah password tersebut!
+                            <br>
+                            <b>3. Memproses Pengajuan IUMK</b><br>
+                            Melansir dari Kompas, berikut ini rangkuman runtut cara membuat surat izin usaha perorangan.
+                            Kembali ke website www.osss.go.id dan masuk dengan akun yang kamu buat tadi.
+
+                            Lalu, klik menu ‘Perizinan Berusaha’ kemudian klik ‘Perseorangan’ dan pilihlah ‘Skala Usaha
+                            Mikro’ dilanjut dengan klik ‘Pendaftaran NIB Perseorangan Mikro’. Langkah selanjutnya, klik
+                            ‘Skala Usaha Kecil’, kemudian pilih ‘Pendaftaran NIB Perseorangan Kecil’.
+
+                            Lakukan pengisian ‘Data Profil’ dan ‘Data Usaha’ dengan sebaik mungkin. Jangan lupa untuk tekan
+                            simpan jika seluruh kolom formulir sudah terisi. Apabila kamu mempunyai lebih dari satu usaha,
+                            bisa melakukan penambahan Data Usaha.
+
+                            Langkah selanjutnya, kamu bisa mengisi formulir ‘Komitmen Prasarana Usaha’ untuk melakukan
+                            pengajuan ‘Izin Lokasi dan Lingkungan’.
+                            <br>
+                            <b>4. Memproses NIB</b><br>
+                            NIB adalah Nomor Induk Berusaha yang menjadi identitas dari pemilik usaha dalam pelaksanaan
+                            kegiatan usahanya. Kamu harus memiliki NIB agar berhasil melakukan pengajuan surat izin usaha
+                            pribadi.
+
+                            Di sesi ini, akan ditampilkan draft NIB dan Izin usaha milikmu. Teliti terlebih dahulu apakah
+                            sudah sesuai. Jika sudah sesuai maka klik centang di kotak disclaimer. Di sini kamu bisa
+                            melakukan cetak Izin Usaha dengan QR code.
+
+                            Demikian tips untuk cara membuat surat izin usaha kecil secara online. Semoga dapat bermanfaat
+                            untuk usaha yang sedang kamu jalankan!
+                            <br><br>
+                            </p>
+                            <div class="post-footer mt-40">
+                                <div class="row justify-content-between">
+                                    <div class="col-auto">
+                                        <div class="related-tags">
+                                            <a href="#">umkm</a>
+                                            <a href="#">funding</a>
+                                            <a href="#">bisnis</a>
+                                        </div>
+                                    </div>
+                                    <div class="col-auto">
+                                        <div class="social-share">
+                                            <span>Share: </span>
+                                            <a href="https://www.facebook.com/sharer.php?u=https://example.com"><i
+                                                    class="fab fa-facebook-f"></i></a>
+                                            <a href="https://twitter.com/intent/tweet?url=https://example.com"><i
+                                                    class="fab fa-twitter"></i></a>
+                                            <a href="https://www.instagram.com/share?url=https://example.com"><i
+                                                    class="fab fa-instagram"></i></a>
+                                        </div>
+                                    </div>
+
+                                </div>
+                                <div class="post-nav mt-50">
+                                    <div class="row">
+                                        <div class="col-sm-6">
+                                            <div class="post-nav-item prev-post">
+                                                <div class="thumb">
+                                                    <img src="../assets/img/blog/prev-post-news1.jpg" alt="Image">
+                                                    <i class="far fa-arrow-left"></i>
+                                                </div>
+                                                <div class="content">
+                                                    <a href="{{ route('news.show', 2) }}">7 Strategi Pengembangan Usaha Agar
+                                                        Semakin Sukses!</a>
+                                                    <span><i class="far fa-calendar-alt"></i> 15 Mei 2023</span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-sm-6">
+                                            <div class="post-nav-item next-post">
+                                                <div class="thumb">
+                                                    <img src="../assets/img/blog/prev-post-news2.png" alt="Image">
+                                                    <i class="far fa-arrow-right"></i>
+                                                </div>
+                                                <div class="content">
+                                                    <a href="{{ route('news.show', 1) }}">6 Strategi dan Tujuan Pemberdayaan
+                                                        UMKM di Indonesia</a>
+                                                    <span><i class="far fa-calendar-alt"></i> 15 Mei 2023</span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--====== Blog Standard Area End ======-->
+@endsection
