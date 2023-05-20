3f695e1b5ec62eba7eba0af04362e364b81be88e
lailatulbadriyah24 <lailatulbadriyah654@gmail.com>
2023-05-20 11:38:54 +0700

    feat: add cara kerja

---
+++ resources/views/pages/cara-kerja-pendana.blade.php
@@ -0,0 +1,116 @@
+@extends('layouts.index')
+@section('container')
+    <!--====== Page Title Start ======-->
+    <section class="page-title-area">
+        <div class="container">
+            <div class="row align-items-center justify-content-between">
+                <div class="col-lg-8">
+                    <h1 class="page-title">Cara Kerja Pendana</h1>
+                </div>
+                <div class="col-auto">
+                    <ul class="page-breadcrumb">
+                        <li><a href="index.html">Home</a></li>
+                        <li>Cara Kerja Pendana</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--====== Page Title End ======-->
+
+    <!--====== Event area Start ======-->
+    <section class="event-area section-gap-extra-bottom" style="padding-bottom: 110px";>
+        <div class="container">
+            <div class="event-items">
+                <div class="single-event-item mb-30 wow fadeInUp" data-wow-delay="0s">
+                    <div class="event-thumb">
+                        <img src="assets/img/event/daftar.png" alt="Image">
+                    </div>
+                    <div class="event-content">
+                        <h4 class="event-title"><a href="#">Pendaftaran Pendana</a></h4>
+                        <p>
+                            Pendana yang ingin menggunakan layanan SaTu harus mendaftar terlebih dahulu. Pendaftaran ini
+                            meliputi pengisian profil. Selain itu, Pendana juga harus menyertakan dokumen pendukung seperti
+                            ktp dan foto selfie untuk proses verifikasi.
+                        </p>
+                    </div>
+                </div>
+                <div class="single-event-item mb-30 wow fadeInUp" data-wow-delay="0.1s">
+                    <div class="event-thumb">
+                        <img src="assets/img/event/verifikasi.png" alt="Image">
+                    </div>
+                    <div class="event-content">
+                        <h4 class="event-title"><a href="#">Pengecekan dan Verifikasi</a></h4>
+                        <p>
+                            Setelah pendaftaran, tim SaTu akan melakukan pengecekan dan verifikasi terhadap profil Pendana
+                            serta
+                            dokumen pendukung yang disertakan. Jika dinyatakan lulus verifikasi, maka telah terdaftar
+                            menjadi Pendana.
+                        </p>
+                    </div>
+                </div>
+                <div class="single-event-item mb-30 wow fadeInUp" data-wow-delay="0.2s">
+                    <div class="event-thumb">
+                        <img src="assets/img/event/campaign.png" alt="Image">
+                    </div>
+                    <div class="event-content">
+                        <h4 class="event-title"><a href="#">Penjelajahan dan Pemilihan Kampanye</a></h4>
+                        <p>
+                            Setelah lulus verifikasi, Pendana dapat menjelajah kampanye yang tersedia. Pada kampanye,
+                            Pendana dapat melihat deskripsi kampanye serta informasi terkait kampanye lainnya.
+                            Kemudian Pendana dapat memilih kampanye yang ingin didukung berdasarkan minat, kepercayaan, dan
+                            tujuan Pendana sendiri.
+                    </div>
+                </div>
+                <div class="single-event-item mb-30 wow fadeInUp" data-wow-delay="0.3s">
+                    <div class="event-thumb">
+                        <img src="assets/img/event/nominal.png" alt="Image">
+                    </div>
+                    <div class="event-content">
+                        <h4 class="event-title"><a href="#">Menentukan Nominal Pendanaan</a></h4>
+                        <p>
+                            Setelah memilih kampanye yang ingin didukung, sebagai Pendana di platform SaTu, langkah
+                            berikutnya adalah menentukan jumlah dana yang ingin diberikan. Pendana memiliki fleksibilitas
+                            untuk menentukan jumlah dukungan sesuai dengan kemampuan dan keinginan. Dengan memilih jumlah
+                            dana yang sesuai, Pendana dapat memberikan kontribusi yang signifikan
+                            dalam mewujudkan proyek atau ide yang ingin direalisasikan oleh Mitra UMKM.
+                        </p>
+                    </div>
+                </div>
+                <div class="single-event-item mb-30 wow fadeInUp" data-wow-delay="0.4s">
+                    <div class="event-thumb">
+                        <img src="assets/img/event/proses.png" alt="Image">
+                    </div>
+                    <div class="event-content">
+                        <h4 class="event-title"><a href="#">Proses Pendanaan</a></h4>
+                        <p>
+                            SaTu menyediakan berbagai opsi pembayaran yang nyaman, seperti transfer bank, kartu kredit, atau
+                            dompet digital, sehingga Pendana dapat memilih metode yang sesuai dengan preferensi mereka.
+                            Setelah memilih metode pembayaran, Pendana perlu mengikuti petunjuk dan melengkapi proses
+                            pembayaran untuk menyelesaikan pendanaan kampanye yang dipilih.
+                        </p>
+                    </div>
+                </div>
+                <div class="single-event-item mb-30 wow fadeInUp" data-wow-delay="0.4s">
+                    <div class="event-thumb">
+                        <img src="assets/img/event/monitoring.png" alt="Image">
+                    </div>
+                    <div class="event-content">
+                        <h4 class="event-title"><a href="#">Memantau Kampanye Yang Didanai</a></h4>
+                        <p>
+
+                            Setelah melakukan pendanaan pada kampanye, penting untuk memantau perkembangan
+                            kampanye yang didukung. Pendana dapat dengan mudah memperoleh pembaruan dan informasi terkini
+                            melalui fitur pemantauan yang disediakan oleh SaTu. Melalui pemantauan kampanye, Pendana dapat
+                            mengetahui progres kampanye, penggunaan dana yang telah terkumpul, serta apakah kampanye sudah
+                            mencapai target atau tidak. Dengan memantau kampanye, Pendana dapat memastikan bahwa dana yang
+                            telah diberikan digunakan dengan transparan dan sesuai dengan tujuan yang diumumkan oleh pendiri
+                            kampanye yaitu Mitra UMKM.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--====== Event area End ======-->
+@endsection
