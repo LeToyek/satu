b969aa82a1aadff92a1e84aaec92b6d413556e7b
lailatulbadriyah24 <lailatulbadriyah654@gmail.com>
2023-05-21 17:03:50 +0700

    feat: add page kebijakan privasi

---
+++ resources/views/pages/kebijakan-privasi.blade.php
@@ -0,0 +1,81 @@
+@extends('layouts.index')
+@section('container')
+    <!--====== Page Title Start ======-->
+    <section class="page-title-area">
+        <div class="container">
+            <div class="row align-items-center justify-content-between">
+                <div class="col-lg-8">
+                    <h1 class="page-title">Kebijakan Privasi</h1>
+                </div>
+                <div class="col-auto">
+                    <ul class="page-breadcrumb">
+                        <li><a href="index.html">Home</a></li>
+                        <li>Kebijakan Privasi</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--====== Page Title End ======-->
+
+    <!--====== Event area Start ======-->
+    <section class="event-area section-gap-extra-bottom" style="padding-bottom: 110px; padding-top: 60px;";>
+        <div class="container">
+            <div class="event-items">
+                <div class="single-event-item mb-30 wow fadeInUp" data-wow-delay="0.1s">
+                    <div class="event-content">
+                        <p>Kami di SaTu menghargai privasi Anda sebagai pengguna platform kami. Dokumen Kebijakan Privasi
+                            ini menjelaskan bagaimana kami mengumpulkan, menggunakan, dan melindungi informasi pribadi Anda.
+                            Kami mendorong Anda untuk membaca kebijakan ini dengan seksama agar memahami bagaimana kami
+                            memperlakukan informasi pribadi Anda.</p>
+
+                        <h4 style="margin-top: 20px; margin-bottom: 10px;">Informasi yang Kami Kumpulkan</h4>
+                        <ul>
+                            <li>1. SaTu dapat mengumpulkan informasi pribadi yang Anda berikan saat mendaftar, menggunakan,
+                                atau berinteraksi dengan platform kami. Informasi ini mencakup nama, alamat email, nomor
+                                telepon, dan informasi terkait lainnya yang diperlukan untuk menyediakan layanan kami dengan
+                                baik.</li>
+                            <li>2. Kami juga dapat mengumpulkan informasi non-pribadi seperti data penggunaan, alamat IP,
+                                cookie, dan informasi teknis lainnya saat Anda mengakses atau berinteraksi dengan platform
+                                SaTu.</li>
+                        </ul>
+
+                        <h4 style="margin-top: 20px; margin-bottom: 10px;">Penggunaan Informasi</h4>
+                        <ul>
+                            <li>1. Informasi pribadi yang kami kumpulkan digunakan untuk mengelola akun Anda, menyediakan
+                                layanan SaTu, memproses transaksi, dan memberikan pembaruan terkait platform.</li>
+                            <li>2. Kami juga dapat menggunakan informasi untuk mengoptimalkan pengalaman pengguna, melakukan
+                                analisis statistik, dan mengembangkan fitur dan layanan baru di SaTu.</li>
+                            <li>Kami tidak akan menjual atau menyewakan informasi pribadi Anda kepada pihak ketiga tanpa
+                                izin Anda, kecuali jika diwajibkan oleh hukum atau dengan persetujuan tertulis dari Anda.
+                            </li>
+                        </ul>
+
+                        <h4 style="margin-top: 20px; margin-bottom: 10px;">Keamanan Informasi</h4>
+                        <ul>
+                            <li>1. Kami mengambil langkah-langkah keamanan yang wajar untuk melindungi informasi pribadi
+                                Anda dari akses yang tidak sah, penggunaan, atau pengungkapan yang tidak sah.</li>
+                            <li>2. Meskipun demikian, tidak ada metode pengiriman data melalui internet yang sepenuhnya
+                                aman. Oleh karena itu, kami tidak dapat menjamin keamanan absolut informasi yang Anda
+                                kirimkan melalui platform SaTu.</li>
+                        </ul>
+
+                        <h4 style="margin-top: 20px; margin-bottom: 10px;">Perubahan Kebijakan Privasi</h4>
+                        <ul>
+                            <li>1. SaTu dapat memperbarui Kebijakan Privasi ini dari waktu ke waktu. Kami akan
+                                menginformasikan perubahan tersebut melalui pemberitahuan di situs web atau melalui
+                                komunikasi langsung kepada pengguna yang relevan.</li>
+                            <li>2. Dengan tetap menggunakan platform SaTu setelah perubahan kebijakan ini, Anda secara
+                                otomatis menyetujui versi terbaru dari Kebijakan Privasi kami.</li>
+                        </ul>
+
+                        <p style="margin-top: 20px; margin-bottom: 10px;">Kami berkomitmen untuk menjaga kerahasiaan dan keamanan informasi pribadi Anda. Jika Anda
+                            memiliki pertanyaan lebih lanjut tentang Kebijakan Privasi ini, jangan ragu untuk menghubungi
+                            kami melalui halaman Kontak kami.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--====== Event area End ======-->
+@endsection
