e8ca6ad373e6838d318460a3e950c2d382fb9eba
lailatulbadriyah24 <lailatulbadriyah654@gmail.com>
2023-05-21 17:04:07 +0700

    feat: add page syarat dan ketentuan

---
+++ resources/views/pages/syarat-ketentuan.blade.php
@@ -0,0 +1,95 @@
+@extends('layouts.index')
+@section('container')
+    <!--====== Page Title Start ======-->
+    <section class="page-title-area">
+        <div class="container">
+            <div class="row align-items-center justify-content-between">
+                <div class="col-lg-8">
+                    <h1 class="page-title">Syarat dan Ketentuan</h1>
+                </div>
+                <div class="col-auto">
+                    <ul class="page-breadcrumb">
+                        <li><a href="index.html">Home</a></li>
+                        <li>Syarat dan Ketentuan</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--====== Page Title End ======-->
+
+    <!--====== Event area Start ======-->
+    <section class="event-area section-gap-extra-bottom" style="padding-bottom: 110px; padding-top: 60px;";>
+        <div class="container">
+            <div class="event-items">
+                <div class="single-event-item mb-30 wow fadeInUp" data-wow-delay="0.1s">
+                    <div class="event-content">
+                        <p>Mohon membaca dengan seksama Syarat dan Ketentuan ini sebelum menggunakan platform SaTu. Dengan
+                            mengakses dan menggunakan platform SaTu, Anda dianggap telah membaca, memahami, dan menyetujui
+                            Syarat dan Ketentuan yang tercantum di bawah ini:</p>
+
+                        <h4 style="margin-top: 20px; margin-bottom: 10px;">Penggunaan Platform</h4>
+                        <ul>
+                            <li>1. SaTu adalah platform crowdfunding yang memungkinkan Mitra UMKM untuk mengumpulkan dana
+                                dari
+                                Pendana.</li>
+                            <li>2. Dilarang menggunakan platform SaTu untuk tujuan yang melanggar hukum atau melanggar
+                                hak-hak
+                                orang lain.</li>
+                            <li>3. Anda bertanggung jawab atas segala kegiatan yang dilakukan melalui akun Anda di SaTu.
+                            </li>
+                        </ul>
+
+                        <h4 style="margin-top: 20px; margin-bottom: 10px;">Ketergantungan pada Informasi</h4>
+                        <ul>
+                            <li>1. Informasi yang disediakan dalam platform SaTu, termasuk deskripsi kampanye, tujuan
+                                penggalangan dana, dan informasi lainnya, merupakan tanggung jawab pendiri kampanye.</li>
+                            <li>2. Kami tidak menjamin kebenaran, kelengkapan, atau keandalan informasi yang disediakan oleh
+                                pendiri kampanye.</li>
+                        </ul>
+
+                        <h4 style="margin-top: 20px; margin-bottom: 10px;">Pendanaan dan Pembayaran</h4>
+                        <ul>
+                            <li>1. SaTu memberikan fasilitas pendanaan melalui metode pembayaran yang disediakan.</li>
+                            <li>2. Anda bertanggung jawab atas keakuratan informasi pembayaran yang Anda berikan.</li>
+                            <li>3. Setiap pendanaan yang telah dilakukan bersifat final, kecuali ditentukan sebaliknya dalam
+                                kebijakan pembatalan yang ditetapkan oleh pendiri kampanye.</li>
+                        </ul>
+
+                        <h4 style="margin-top: 20px; margin-bottom: 10px;">Privasi dan Keamanan</h4>
+                        <ul>
+                            <li>1. Kami menghormati privasi Anda sebagai pengguna SaTu. Informasi pribadi yang Anda berikan
+                                akan diatur sesuai dengan Kebijakan Privasi kami.</li>
+                            <li>2. Kami mengambil langkah-langkah keamanan yang wajar untuk melindungi informasi Anda, namun
+                                tidak dapat menjamin keamanan mutlak terhadap ancaman dari luar.</li>
+                        </ul>
+
+                        <h4 style="margin-top: 20px; margin-bottom: 10px;">Perubahan dan Penghentian Layanan</h4>
+                        <ul>
+                            <li>1. SaTu berhak untuk melakukan perubahan pada platform, termasuk fitur, tampilan, dan
+                                layanan
+                                yang disediakan.</li>
+                            <li>2. Kami juga berhak untuk menghentikan atau membatasi akses Anda ke platform SaTu tanpa
+                                pemberitahuan sebelumnya.</li>
+                        </ul>
+
+                        <h4 style="margin-top: 20px; margin-bottom: 10px;">Tanggung Jawab Hukum</h4>
+                        <ul>
+                            <li>1. Kami tidak bertanggung jawab atas kerugian atau kerusakan yang timbul akibat penggunaan
+                                platform SaTu.</li>
+                            <li>2. Anda setuju untuk membebaskan SaTu dari segala tuntutan atau klaim hukum yang timbul dari
+                                penggunaan platform oleh Anda.</li>
+                        </ul>
+
+                        <h4  style="margin-top: 20px; margin-bottom: 10px;">Hukum yang Berlaku</h4>
+                        <p>1. Syarat dan Ketentuan ini tunduk pada hukum yang berlaku di wilayah yang berwenang.</p>
+
+                        <p>Jika Anda memiliki pertanyaan lebih lanjut mengenai Syarat dan Ketentuan ini, silakan hubungi
+                            kami melalui halaman <a href="/contact-us">Kontak kami</a>.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--====== Event area End ======-->
+@endsection
