ba19100fd05aadc02ae0e11b6334c1d796ee1b45
Tio Misbaqul Irawan <sattanationmail@gmail.com>
2023-05-22 06:35:01 +0000

    feat: funder registration controller

--- app/Http/Controllers/Auth/Registration/FunderRegistrationController.php
+++ app/Http/Controllers/Auth/Registration/FunderRegistrationController.php
@@ -7,5 +7,34 @@ use Illuminate\Http\Request;
 
 class FunderRegistrationController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('auth.role.register_funder');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'ksei_number' => 'required|max:255',
+        ]);
+
+        $user =  auth()->user();
+
+        $user->details()->firstOrCreate($request->except(['_token']));
+
+        return redirect()->route('dashboard.index')->with('success', 'Berhasil mendafar');
+    }
+
+    public function update(Request $request)
+    {
+        $request->validate([
+            'ksei_number' => 'required|max:255',
+        ]);
+
+        $user = auth()->user();
+
+        $user->details()->update($request->except(['_token']));
+
+        return redirect()->route('dashboard.index')->with('success', 'Berhasil mendafar');
+    }
 }
