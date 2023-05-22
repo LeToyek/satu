e77f3d178bc2e954a0596d244c171df423ca5dbb
Tio Misbaqul Irawan <sattanationmail@gmail.com>
2023-05-22 05:50:44 +0000

    feat: partner registration

--- app/Models/Document.php
+++ app/Models/Document.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
+use Illuminate\Support\Facades\Storage;
 
 class Document extends Model
 {
@@ -20,5 +21,10 @@ class Document extends Model
     public function documentable(): MorphTo
     {
         return $this->morphTo();
+    }
+
+    public function getUrlAttribute(): string
+    {
+        return Storage::disk('public')->url($this->path);
     }
 }
