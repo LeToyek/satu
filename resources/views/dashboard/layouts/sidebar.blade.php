12b3b0b9c2785bcecc5e0f0fc5d08bd8ec9282c0
Maulana Arif Wijaya <maulanatoyek@gmail.com>
2023-05-26 15:08:19 +0700

    chore: change logo

--- resources/views/dashboard/layouts/sidebar.blade.php
+++ resources/views/dashboard/layouts/sidebar.blade.php
@@ -5,19 +5,19 @@
         <!-- Dark Logo-->
         <a href="{{ url('/dashboard') }}" class="logo logo-dark">
             <span class="logo-sm">
-                <img src="{{ URL::asset('velzon/images/logo-sm.png') }}" alt="" height="22">
+                <img src="{{ URL::asset('assets/img/logo-satu.svg') }}" alt="" height="32">
             </span>
             <span class="logo-lg">
-                <img src="{{ URL::asset('velzon/images/logo-dark.png') }}" alt="" height="17">
+                <img src="{{ URL::asset('assets/img/logo-satu.svg') }}" alt="" height="24">
             </span>
         </a>
         <!-- Light Logo-->
         <a href="{{ url('/dashboard') }}" class="logo logo-light">
             <span class="logo-sm">
-                <img src="{{ URL::asset('velzon/images/logo-sm.png') }}" alt="" height="22">
+                <img src="{{ URL::asset('assets/img/logo-satu.svg') }}" alt="" height="32">
             </span>
             <span class="logo-lg">
-                <img src="{{ URL::asset('velzon/images/logo-light.png') }}" alt="" height="17">
+                <img src="{{ URL::asset('assets/img/logo-satu.svg') }}" alt="" height="24">
             </span>
         </a>
         <button type="button" class="btn btn-sm p-0 fs-20 header-item float-end btn-vertical-sm-hover" id="vertical-hover">
