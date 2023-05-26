29720aee3b67dd1e4a6f51e5824ba1a592d054a8
Maulana Arif Wijaya <maulanatoyek@gmail.com>
2023-05-26 15:04:52 +0700

    feat: init status component

---
+++ resources/views/dashboard/components/status_funding.blade.php
@@ -0,0 +1,9 @@
+@if ($status == 'on_going')
+    <td><span class="badge bg-primary">On Going</span></td>
+@elseif ($status == 'repaid')
+    <td><span class="badge bg-info">Waiting for return</span></td>
+@elseif ($status == 'on_sell')
+    <td><span class="badge bg-warning">On Sell</span></td>
+@elseif ($status == 'failed')
+    <td><span class="badge bg-danger">Return Distribution</span></td>
+@endif
