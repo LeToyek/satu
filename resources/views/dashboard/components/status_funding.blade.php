6fcf7f1ed8ebbc7d6fadb046f2bbffe78a7fb383
Maulana Arif Wijaya <maulanatoyek@gmail.com>
2023-05-29 13:38:51 +0700

    feat: add new status

--- resources/views/dashboard/components/status_funding.blade.php
+++ resources/views/dashboard/components/status_funding.blade.php
@@ -4,6 +4,8 @@
     <td><span class="badge bg-info">Waiting for return</span></td>
 @elseif ($status == 'on_sell')
     <td><span class="badge bg-warning">On Sell</span></td>
+@elseif ($status == 'unclaimed')
+    <td><span class="badge bg-secondary">Unclaimed</span></td>
 @elseif ($status == 'failed')
     <td><span class="badge bg-danger">Return Distribution</span></td>
 @endif
