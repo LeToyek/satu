761d370da374c38b945cfc5d57a38efbeb548297
Tio Misbaqul Irawan <sattanationmail@gmail.com>
2023-07-04 10:44:16 +0000

    feat: force https on production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 
@@ -23,5 +24,10 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         Paginator::useBootstrapFive();
+
+        // if in production, force https
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
